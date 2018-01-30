9019bdadee99cd3bf6831057cc8d68303a07dc19
Antonio Figueiredo <antonio.figueiredo@olx.com>
2018-01-30 17:13:32 +0100

    add setup to all tests

--- tests/PlaceOrderCenarioTest.php
+++ tests/PlaceOrderCenarioTest.php
@@ -12,27 +12,31 @@ use PHPUnit\Framework\TestCase;
 class PlaceOrderCenarioTest extends TestCase
 {
     /**
-     * @throws \Exception
+     * @var TestScenario
      */
+    public $scenario;
+
+    public function setUp()
+    {
+        $this->scenario = new TestScenario();
+    }
+
     public function testPlacedOrder()
     {
-
         $customerId = '1';
-        $cartId = '2222';
-        $orderTime = '2018-01-01 03:03:03';
-        $sku = "0000-aaaa-bbb";
-        $price = "13,00";
-        $addedAt = "2018-01-01 02:03:03";
+        $cartId     = '2222';
+        $orderTime  = '2018-01-01 03:03:03';
+        $sku        = "0000-aaaa-bbb";
+        $price      = "13,00";
+        $addedAt    = "2018-01-01 02:03:03";
 
         $products = [];
 
-        $cenario = new TestScenario();
-
-        $cenario
+        $this->scenario
             ->given(new CustomerStartedShopping($customerId, $cartId))
-            ->given(new ProductWasAddedToCart($customerId, $cartId, $sku,$price, $addedAt))
+            ->given(new ProductWasAddedToCart($customerId, $cartId, $sku, $price, $addedAt))
             ->when(new PlaceOrder($customerId, $cartId, $orderTime))
-            ->then(new CustomerPlacedOrder($customerId,  $cartId, $products))
+            ->then(new CustomerPlacedOrder($customerId, $cartId, $products))
             ->assert();
     }
 }
--- tests/StartShoppingTest.php
+++ tests/StartShoppingTest.php
@@ -2,18 +2,29 @@
 
 namespace Diy;
 
+use Diy\Tools\TestScenario;
 use PHPUnit\Framework\TestCase;
 
 class StartShoppingTest extends TestCase
 {
+    /**
+     * @var TestScenario
+     */
+    public $scenario;
+
+    public function setUp()
+    {
+        $this->scenario = new TestScenario();
+    }
+
     public function testStartShopping()
     {
         $customerId = 2;
         $cartId = '1';
         $startTime = '2018-01-01 03:14:45';
 
-        $scenario = new Tools\TestScenario();
-        $scenario
+
+        $this->scenario
             ->when(
                 new Domain\Commands\StartShopping($customerId, $cartId, $startTime)
             )
